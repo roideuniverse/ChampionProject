aa6b404ab8b87dd811ba5e67bc28dd0aad739b4b
Kaushik Saurabh <roideuniverse@gmail.com>
2015-07-26 18:37:41 -0400

    Tag name unique and required

--- api/models/Tag.js
+++ api/models/Tag.js
@@ -9,7 +9,9 @@ module.exports = {
 
   attributes: {
     name: {
-      type: 'string'
+        type: 'string',
+        required: true,
+        unique: true
     },
 
     //add reference to Notes
